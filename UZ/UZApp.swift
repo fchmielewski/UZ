01b40e45dcf1468e6d5115cfdbc2a8d1f91da09c
Filip Chmielewski <filipchmielewski@icloud.com>
2022-09-09 13:33:26 +0200

    Initial Commit

---
+++ UZ/UZApp.swift
@@ -0,0 +1,17 @@
+//
+//  UZApp.swift
+//  UZ
+//
+//  Created by Filip Chmielewski on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct UZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
